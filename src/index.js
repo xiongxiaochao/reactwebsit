bbe7917acdf4af7b07a43d7bbcace5613518584f
xiong <xiongxiaochao@foresee.com.cn>
2019-12-27 09:42:14 +0800

    完成井字棋

--- src/index.js
+++ src/index.js
@@ -35,6 +35,7 @@ function Square(props) {
           super(props)
           this.state = {
               squares: Array(9).fill(null),
+              xIsNext: true,
           }
       }
     renderSquare(i) {
@@ -65,12 +66,25 @@ function Square(props) {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squres: squres});
+        if(calculateWinner(squares) || squares[i]){
+          return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+          squares: squares,
+          xIsNext: !this.state.xIsNext,
+        });
     }
   
     render() {
-      const status = 'Next player: X';
+      // const status = 'Next player:' + (this.state.xIsNext ? 'X' : 'O');
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if(winner) {
+        status = 'Winner:' + winner; 
+      }else{
+        status = 'Next player:' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
@@ -109,6 +123,26 @@ function Square(props) {
         </div>
       );
     }
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
   
   // ========================================
